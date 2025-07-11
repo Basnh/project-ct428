613fb32f493167b83d9babd5e75954dfd594f799
DNA <anhb2204914@student.ctu.edu.vn>
2025-07-11 08:11:42 +0700

    edited db.php

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ $passwd = "";
 $dbname = "todo-list"; 
 
 // Tạo kết nối
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $passwd, $dbname);
 
 // Kiểm tra kết nối
 if ($conn->connect_error) {
